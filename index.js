1aee0b882aba0017fb945e4fdf20006b559360d2
Nick Currie <gosox997@gmail.com>
2023-04-25 22:46:54 -0400

    that was tough!

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        const p = document.createElement('p')
+        document.body.append(p)
+        p.textContent = `${object.id}`
+
+    })
+    .catch(function (error) {
+        alert("Hmmmmm, Something went wrong mate!");
+        console.log(error.message);
+        const errorMsg = document.createElement('div')
+        errorMsg.textContent = `${error.message}`
+        document.body.append(errorMsg);
+    });
+}
